aa9e4f2d388915e9819e8e6e18fc42094b7ca4a7
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-02 10:42:27 +0530

    sol: Multiply without using arithmatic operation

---
+++ primitive/src/MultWithoutArith.java
@@ -0,0 +1,18 @@
+public class MultWithoutArith {
+
+    public static void main(String[] args) {
+        System.out.println(multiply(13, 9));
+    }
+
+    public static long multiply(long x , long y){
+        long sum = 0;
+        while(x!=0){
+            if((x&1) != 0) {
+                sum += y;
+            }
+            x>>>=1;
+            y<<=1;
+        }
+        return  sum;
+    }
+}
