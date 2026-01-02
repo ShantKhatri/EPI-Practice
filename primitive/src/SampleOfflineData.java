522034fe3881a69f7c66cac695d1bbdcd1c3b182
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-02 10:54:13 +0530

    sol: Sample OfflineData

---
+++ primitive/src/SampleOfflineData.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class SampleOfflineData {
+
+    public static void randomSampling(int k, List<Integer> A) {
+        Random gen = new Random();
+        for (int i = 0; i < k; i++) {
+            Collections.swap(A, i, i + gen.nextInt(A.size() - i));
+        }
+    }
+}
