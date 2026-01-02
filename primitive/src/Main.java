80d380b94578d16eb088ba6732e1102ac0525409
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-02 10:40:28 +0530

    Project Initialization

---
+++ primitive/src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.println(countBit(-29));
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+
+    public static short countBit(int x) {
+        if(x<0){
+            x = -x;
+        }
+        short numBits = 0;
+        while(x!=0){
+            numBits += (short) (x & 1);
+            x >>= 1;
+        }
+        return numBits;
+    }
+}
