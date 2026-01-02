2aebedcc94c883e92c1a49d742c9c5e3af694ec5
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-02 15:33:15 +0530

    sol: Generate NonUniform Random Numbers

---
+++ primitive/src/GenerateNonUniformRandNumbers.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class GenerateNonUniformRandNumbers {
+
+    public static int nonuniformRandomNumberGenerator(List<Integer> values, List<Double> probabilities) {
+        List<Double> psp = new ArrayList<>();
+        psp.add(0.0);
+        for (Double p : probabilities)
+            psp.add(psp.get(psp.size()-1));
+        Random r = new Random();
+        final Double uniform01 = r.nextDouble();
+        int it = Collections.binarySearch(psp, uniform01);
+        if (it < 0) {
+            final int intervalIdx = (Math.abs(it)-1)-1;
+            return values.get(intervalIdx);
+        } else {
+            return values.get(it);
+        }
+    }
+}
