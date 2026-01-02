6504b64219a6fbc4e285fcb7e0449f3f98970ad5
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-02 14:58:02 +0530

    sol: Compute the Random Permutation

---
+++ primitive/src/ComputeRandomPermutation.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputeRandomPermutation {
+
+    public static List<Integer> computeRandomPermutation(int n) {
+        List<Integer> permutation = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            permutation.add(i);
+        }
+        SampleOfflineData.randomSampling(permutation.size(), permutation);
+        return permutation;
+    }
+}
