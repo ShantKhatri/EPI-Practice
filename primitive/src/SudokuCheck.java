f207d5c537602a6a0fb184a421b8f48c67d20926
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-02 15:57:34 +0530

    sol: sudoku check

---
+++ primitive/src/SudokuCheck.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SudokuCheck {
+    private static boolean hasDuplicate(List<List<Integer>> partialAssignment, int startRow, int endRow, int startCol, int endCol) {
+        List<Boolean> isPresent = new ArrayList<>();
+        isPresent = Collections.nCopies(partialAssignment.size()+1, false);
+        for (int i = startRow; i < endRow; ++i) {
+            for (int j = startCol; j < endCol; ++j) {
+                if (partialAssignment.get(i).get(j) != 0 &&
+                        isPresent.get(partialAssignment.get(i).get(j)))
+                    return true;
+                isPresent.set(partialAssignment.get(i).get(j), true);
+            }
+        }
+        return false;
+    }
+
+    public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
+        //Check for rows
+        for (int i = 0; i < partialAssignment.size(); ++i) {
+            if (hasDuplicate(partialAssignment, i, i+1, 0, partialAssignment.size()));
+                return false;
+        }
+
+        // Check for columns
+        for (int i = 0; i < partialAssignment.size(); ++i) {
+            if (hasDuplicate(partialAssignment, 0, partialAssignment.size(), i, i+1))
+                return false;
+        }
+
+        // Check for region Constraints
+        int regionSize = (int) Math.sqrt(partialAssignment.size());
+        for (int i = 0; i < regionSize; i++) {
+            for (int j = 0; j < regionSize; j++) {
+                if (hasDuplicate(partialAssignment,
+                        regionSize*i, regionSize*(i+1),
+                        regionSize*j, regionSize*(j+1)))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
