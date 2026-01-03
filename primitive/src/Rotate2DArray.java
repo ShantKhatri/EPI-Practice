9516bf9f6983db4189a128c7ac99c1f0382ad991
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-03 17:28:12 +0530

    sol: Rotate 2D array

---
+++ primitive/src/Rotate2DArray.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rotate2DArray {
+
+    public static void rotateMatrix(List<List<Integer>> squareMatrix) {
+        final int matrixSize = squareMatrix.size()-1;
+        for (int i = 0; i < (squareMatrix.size() / 2); i++) {
+            for (int j = 0; j < matrixSize - i; j++) {
+                int temp1 = squareMatrix.get(i).get(j);
+                int temp2 = squareMatrix.get(j).get(matrixSize - i);
+                int temp3 = squareMatrix.get(matrixSize-i).get(matrixSize-j);
+                int temp4 = squareMatrix.get(matrixSize-j).get(i);
+                squareMatrix.get(i).set(matrixSize-j, temp1);
+                squareMatrix.get(matrixSize-j).set(matrixSize-i, temp2);
+                squareMatrix.get(matrixSize-i).set(j, temp3);
+                squareMatrix.get(i).set(j, temp4);
+            }
+        }
+    }
+}
