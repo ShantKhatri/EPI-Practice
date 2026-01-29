1c2344024b2a678e4041877b84cafc123fef55c7
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2026-01-29 16:27:05 +0530

    sol: Find Root to leaf path with specified sum, return all the paths

--- primitive/src/binarytrees/HasPathSum.java
+++ primitive/src/binarytrees/HasPathSum.java
@@ -1,5 +1,8 @@
 package binarytrees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HasPathSum {
 
     public static boolean hasPathSum(BinaryTreeNode<Integer> root, int target) {
@@ -14,5 +17,30 @@ public class HasPathSum {
         if (node.left == null && node.right == null) return sum == target;
         return dfsForPathSum(node.left, sum, target)
                 && dfsForPathSum(node.right, sum, target);
+    }
+
+    public static List<List<Integer>> pathWithTargetSum(BinaryTreeNode<Integer> root, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        pathWithTargetSumHelper(root, 0, target, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void pathWithTargetSumHelper(BinaryTreeNode<Integer> node,
+                                                int sum,
+                                                int target,
+                                                List<Integer> curr_list,
+                                                List<List<Integer>> result) {
+        if (node == null) return;
+
+        sum += node.data;
+        curr_list.add(node.data);
+        if (node.left == null && node.right == null) {
+            if (sum == target) result.add(new ArrayList<>(curr_list));
+        } else {
+            pathWithTargetSumHelper(node.left, sum, target, curr_list, result);
+            pathWithTargetSumHelper(node.right, sum, target, curr_list, result);
+        }
+
+        curr_list.removeLast();
     }
 }
